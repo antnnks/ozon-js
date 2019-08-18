d4d91941303ee8b98ce966a70ef2c020b5b62139
antnnks <49480124+antnnks@users.noreply.github.com>
2019-08-18 16:06:05 +0300

    lesson day-2.part-2

--- src/index.js
+++ src/index.js
@@ -104,28 +104,63 @@ function addCart() {
 function actionPage() {
 
     const cards = document.querySelectorAll('.goods .card'),
-    discountCheckbox = document.getElementById('discount-checkbox');
+        discountCheckbox = document.getElementById('discount-checkbox'),
+        goods = document.querySelector('.goods'),
+        min = document.getElementById('min'),
+        max = document.getElementById('max'),
+        search = document.querySelector('.search-wrapper_input'),
+        searchBtn = document.querySelector('.search-btn');
 
+//фильтр по акции
     discountCheckbox.addEventListener('click', () => {
         cards.forEach((card) => {
-            if (discountCheckbox.checked){
-                if (!card.querySelector('.card-sale')){
-                    card.parentNode.style.display = 'none';
+            if (discountCheckbox.checked) {
+                if (!card.querySelector('.card-sale')) {
+                    card.parentNode.remove();
+                    // card.parentNode.style.display = 'none';
                 }
             } else {
-                card.parentNode.style.display = '';
+                goods.appendChild(card.parentNode);
+                // card.parentNode.style.display = '';
             }
         });
     });
 
+//фильтр по цене
+    min.addEventListener('change', filterPrice);
+    max.addEventListener('change', filterPrice);
 
+    function filterPrice() {
+        cards.forEach((card) => {
+            const cardPrice = card.querySelector('.card-price');
+            const price = parseFloat(cardPrice.textContent);
+
+            if ((min.value && price < min.value) || (max.value && price > max.value)) {
+                card.parentNode.remove();
+                // card.parentNode.style.display = 'none';
+            } else {
+                goods.appendChild(card.parentNode);
+                //card.parentNode.style.display = '';
+            }
+        });
+    }
+
+//поиск
+    searchBtn.addEventListener('click', () => {
+        const searchText = new RegExp(search.value.trim(), 'i');
+        cards.forEach((card) => {
+            const title = card.querySelector('.card-title');
+            if (!searchText.test(title.textContent)) {
+                card.parentNode.style.display = 'none';
+            } else {
+                card.parentNode.style.display = '';
+            }
+        });
+        search.value = '';
+    });
 }
 
-
-
-
 //end фильтр акции
-
 
 toggleCheckbox();
 toggleCart();
